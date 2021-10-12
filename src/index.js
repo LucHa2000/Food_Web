0d628f179abff8398246dfc4d4ff9bea9fba32b4
Nhan Dang <nhan.icdi@gmail.com>
2021-10-12 22:46:04 +0700

    fixed cart UI

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const cookieParser = require('cookie-parser'); //use cookies
-const MongoStore = require('connect-mongo'); //session with mongoDB
-const session = require('express-session') //session
-const flash = require('connect-flash'); //flash session
-const methodOverride = require('method-override'); //override put patch
-const path = require('path'); // tra ve path cua file run
-const handlebars = require('express-handlebars'); // layout
-const morgan = require('morgan'); //get logger client sen
-const nodemailer = require('nodemailer'); //sendEmailConfirm
+const express = require("express");
+const cookieParser = require("cookie-parser"); //use cookies
+const MongoStore = require("connect-mongo"); //session with mongoDB
+const session = require("express-session"); //session
+const flash = require("connect-flash"); //flash session
+const methodOverride = require("method-override"); //override put patch
+const path = require("path"); // tra ve path cua file run
+const handlebars = require("express-handlebars"); // layout
+const morgan = require("morgan"); //get logger client sen
+const nodemailer = require("nodemailer"); //sendEmailConfirm
 
 const moment = require("moment");
 const SortMiddleware = require("./app/middlewares/SortMiddlewares");
@@ -18,21 +18,23 @@ const port = 8000;
 app.use(cookieParser());
 
 //use session
-app.set('trust proxy', 1) // trust first proxy
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  store: MongoStore.create({
-    mongoUrl: 'mongodb://localhost:27017/MedicalDB'
-  }),
-  cookie: {
-    secure: !true
-  }
-}))
-app.use(flash())
-app.use(express.static(path.join(__dirname, 'public'))); //read file static
-app.use(methodOverride('_method'));
+app.set("trust proxy", 1); // trust first proxy
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: "mongodb://localhost:27017/MedicalDB",
+    }),
+    cookie: {
+      secure: !true,
+    },
+  })
+);
+app.use(flash());
+app.use(express.static(path.join(__dirname, "public"))); //read file static
+app.use(methodOverride("_method"));
 
 app.use(express.static(path.join(__dirname, "public"))); //read file static
 app.use(methodOverride("_method"));
